436f194dd4f33d39d6200d2cbaa23a3bdc4ba1b3
herom <jerome.manset22@gmail.com>
2023-02-01 16:30:04 +0800

    add state for dice and map elements to the Die component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,25 @@ import './App.css'
 import Die from './components/Die'
 
 const App = () => {
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice(){
+    const newDice = [];
+        for (let i = 0; i<10; i++){
+        const die = Math.ceil(Math.random() * 6)
+        newDice.push(die)
+      }
+      return newDice
+    }
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
     <div className='main-container'>
-      <Die />
+      <div className='dice-container'>
+      {diceElements}
+      </div>
     </div>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 
-const Die = () => {
-
-    const allDice = [];
-    for (let i = 0; i<10; i++){
-        const die = Math.ceil(Math.random() * 6)
-        allDice.push(die)
-    }
-
-    //console.log(allDice)
-
-    const dieElements = allDice.map(die => <div className='die-container'>{die}</div>)    
-
+const Die = (props) => {
+console.log(props.value)
   return (
-    <div className='dice-container'>
-      {dieElements}
+    <div className='die-container'>
+      {props.value}
     </div>
   )
 }
